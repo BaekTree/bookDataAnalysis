0e6272dbcf1e3134af968b868f2034858e1e2922
baekTree <etknbe@gmail.com>
2021-01-18 12:50:21 +0900

    [lab] test request works for dynamic page

--- book/book/spiders/spider.py
+++ book/book/spiders/spider.py
@@ -1,6 +1,9 @@
 import scrapy
 from bs4 import BeautifulSoup
 import requests
+import json
+from scrapy.http import FormRequest
+
 class BookSpider(scrapy.Spider):
     name = "book"
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
@@ -9,15 +12,70 @@ class BookSpider(scrapy.Spider):
     def start_requests(self):
         user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         headers = {'User-Agent': user_agent}
-        start_url = 'http://www.g2b.go.kr/pt/menu/selectSubFrame.do?framesrc=/pt/menu/frameTgong.do?url=http://www.g2b.go.kr:8101/ep/tbid/tbidList.do?taskClCds=&bidNm=%B5%B5%BC%AD&searchDtType=1&fromBidDt=2020/12/17&toBidDt=2021/01/16&fromOpenBidDt=&toOpenBidDt=&radOrgan=1&instNm=&area=&regYn=Y&bidSearchType=1&searchType=1'
+        start_url = 'http://www.g2b.go.kr:8101/ep/result/facilBidResultDtl.do'
+        ans_rul="http://www.g2b.go.kr:8101/ep/tbid/tbidList.do?taskClCds=&bidNm=%B5%B5%BC%AD&searchDtType=1&fromBidDt=2016/04/15&toBidDt=2016/05/15&fromOpenBidDt=&toOpenBidDt=&radOrgan=1&instNm=&area=&regYn=Y&bidSearchType=1&searchType=1"
 
-        yield scrapy.Request(url = start_url, callback = self.parse, meta={'start_url':start_url})
+        """
+        curl -d "bidno=20160514664&bidseq=00&bidcate=0&rebidno=0&progressInfo=%B0%B3%C2%FB%BF%CF%B7%E1" \
+        -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36" \
+        -H "Origin: http://www.g2b.go.kr:8081" \
+        -H "Content-Type: application/x-www-form-urlencoded" \
+        -H "Referer: http://www.g2b.go.kr:8081/" \
+        -X POST http://www.g2b.go.kr:8101/ep/result/facilBidResultDtl.do | iconv -c -f euc-kr -t utf-8 > output.xml
+        """
 
+        done="개찰완료".encode('euc-kr')
+
+        payload={'bidno': '20160514664', 'bidseq': '00', 'bidcate' : '0', 'rebidno': '0', 'progressInfo':done }
+        yield scrapy.Request(url = start_url, method="POST", body=json.dumps(payload),callback = self.parse)
+
+        
+
+        # return [FormRequest(url="http://www.g2b.go.kr:8101/ep/result/facilBidResultDtl.do",
+        #                 formdata={'bidno': '20160514664', 'bidseq': '00', 'bidcate' : '0', 'rebidno': '0', 'progressInfo':'%B0%B3%C2%FB%BF%CF%B7%E1'},
+        #                 callback=self.parse)]
+
+# https://docs.scrapy.org/en/latest/topics/request-response.html
+
+
+    # def test(self, response):
+        # res=requests.post()
+# {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+# {'Accept-Encoding': 'gzip, deflate',
+# {'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6',
+# {'Cache-Control': 'max-age=0',
+# {'Connection': 'keep-alive',
+# {'Content-Length': '85',
+# {'Content-Type': 'application/x-www-form-urlencoded',
+# {'Cookie': 'ipmsperf_uuid=-3429498447481713841; keywords=%uB3C4%uC11C; ccsession=202101171457180000303f303f07f3; _KONEPS_PAGE=%3C%21%23%25%3EtbidListUrl%3C%40%25%7E%3E%2Fep%2Ftbid%2FtbidList.do%3Farea%3D%26bidNm%3D%25B5%25B5%25BC%25AD%26bidSearchType%3D1%26fromBidDt%3D2016%252F04%252F15%26fromOpenBidDt%3D%26instNm%3D%26radOrgan%3D1%26regYn%3DY%26searchDtType%3D1%26searchType%3D1%26taskClCds%3D%26toBidDt%3D2016%252F05%252F15%26toOpenBidDt%3D; G2B_EP2_PRC=eG1heXhFUWRwd0ZpalFQV2w0UTJPcjNQcTZEWVFQKzV0OFUxWXF2bjRDVkVTbS9mcDNpNzhSdUNpV3VtMVBXbApINUtwMWU5b3cra3lzN0ozeXRZWjFac2dxdk03TmpVRUlHanBwT2tIZlRFNkNzYWJmb3JTbGdlNXdERkJMelpQCmJTNG90ZVpKQ1A1L0lMbGhVdlpMK3ltTkhDQUFEd0l6ekxTZGhRWHpsaFFqcE1jZmlhWFNQbUxYOGFFVjBvQzcKT2s1TGl2TkFlb08rVm9JL29XNEk5ZllVRzZZRG9jaHIwN3FzK2xIaW55ZFV3SzRiTCtYNVN3cmtsYjNaMlNQZwpYOCtLU0JwWXdFS0pkV1Z1dEtMTUdNNmNGS3hCcElzTEh1UEE4RjV5Rlp5dSsxUG5CVFQ0RjAzd2gycURscFVXCmgyMDE4cHZYbHRQU053aCt3dTNhL0E9PUAjJCVeJipKN3RET0FyVENEZUI1MDg4MGdUUVFXcjJFVllxSEtnNi9BVUZvbjY0RGdvTjY4dTlJWm9BdDZzU2VyM3RwWjk3; JSESSIONID=SnKbgGKQQvxNH4rW12LhytzgkGXsTfppk3wNc3fTWdr83CWDWHzL!-1631601383',
+# {'Host': 'www.g2b.go.kr:8101',
+# {'Origin': 'http://www.g2b.go.kr:8081',
+# {'Referer': 'http://www.g2b.go.kr:8081/',
+# {'Upgrade-Insecure-Requests': '1',
+# {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+
+        # return scrapy.FormRequest.from_response(
+        #             response,
+        #             formname='ebid',
+        #             formid='ebid',
+        #             formdata={'bidno': '20160514664', 'bidseq': '00', 'bidcate' : '0', 'rebidno': '0', 'progressInfo':'개찰완료','bidno2': '20160514664', 'bidseq2': '00', 'bidcate2' : '0', 'rebidno2': '0', 'progressInfo2':'개찰완료'},
+        #             callback=self.parse
+        #         )
+                
     def parse(self, response):
     
 
         print("\n\n\n\n---------- THIS IS PARSE RESULT ----------\n")
-        print(response)
+        # print(response.url)
+        # print(response.status)
+        # print(response.headers)
+        # print(response.body)
+        # print(response.flags)
+
+        # print(response.request)
+        # print(response.url)
+        # print(response.url)
+        print(response.xpath("//*").extract())
         # 공고 announcement 파악
         # ans=response.xpath("//*[@id='resultForm']/div[2]/table/tbody/tr[2]/td[4]/div/a[1]/@href").extract()
         # print(ans)
